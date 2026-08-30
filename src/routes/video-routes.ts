9d00c05c09d311d264d12193f5066139404680f1
algasami <105358814+algasami@users.noreply.github.com>
2026-08-30 10:41:12 +0800

    force UPLOAD_DIR to be created upon load

--- src/routes/video-routes.ts
+++ src/routes/video-routes.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import { videoPostController, videoRootController } from "../controllers/video-controller.js";
 import multer from "multer";
 import { randomUUIDv7 } from "crypto";
+import { mkdirSync } from "fs";
+
+const UPLOAD_DIR = "user-uploads";
+mkdirSync(UPLOAD_DIR, { recursive: true });
 
 const upload = multer({
     storage: multer.diskStorage({
-        destination: "user-uploads",
+        destination: UPLOAD_DIR,
         filename(req, file, callback) {
             callback(null, randomUUIDv7());
         },
