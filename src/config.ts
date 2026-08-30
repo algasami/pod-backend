0a5b1204f79723a7b59b7607b233d4c02723ef46
algasami <105358814+algasami@users.noreply.github.com>
2026-08-30 11:21:26 +0800

    restructure configs + use cors

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+import { resolve } from "path";
+
+export const UPLOAD_DIR = resolve(process.cwd(), "user-uploads");
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { errorHandler } from "./middlewares/error-handler.js";
 import { videoRoutes } from "./routes/video-routes.js";
 
 const app = express();
+app.use(cors());
 app.use(videoRoutes);
 
 app.use(errorHandler);
